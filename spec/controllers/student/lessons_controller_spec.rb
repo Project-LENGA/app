e4cba6d489fa4af281f84eb1bb87ebe39a591e03
eiji03aero <eiji03aero@gmail.com>
2018-10-07 18:03:37 +0900

    added spec for controller

--- spec/controllers/student/lessons_controller_spec.rb
+++ spec/controllers/student/lessons_controller_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Student::LessonsController, type: :controller do
+  before(:each) do
+    create(:student)
+  end
 
+  describe 'render' do
+    it 'should render index' do
+      get :index
+      expect(response).to render_template('index')
+    end
+  end
 end
