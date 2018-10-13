4f8ee6df984631313d44731c2f021183e74c97ba
eiji03aero <eiji03aero@gmail.com>
2018-10-13 16:14:00 +0900

    added support for time zone

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true
+  validates :time_zone, inclusion: { in: ActiveSupport::TimeZone.all.map(&:name) }
 end
