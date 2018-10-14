84d904e0902322e7f24e62b43c1e0e0192033e07
Hinatsu Kusano <hinatsukusano@gmail.com>
2018-10-14 22:16:11 +0900

    WIP

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   validates :time_zone, inclusion: { in: ActiveSupport::TimeZone.all.map(&:name) }
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
