001e9276e949bc0c4915ce03533338af87d03494
eiji03aero <eiji03aero@gmail.com>
2018-11-06 07:48:22 +0900

    modified migration file

--- db/migrate/20181105115255_add_studnet_activated_and_tutor_activated_to_users.rb
+++ db/migrate/20181105115255_add_studnet_activated_and_tutor_activated_to_users.rb
@@ -1,6 +1,6 @@
 class AddStudnetActivatedAndTutorActivatedToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :student_activated, :bool, default: false
-    add_column :users, :tutor_activated, :bool, default: false
+    add_column :users, :student_activated, :boolean, default: false
+    add_column :users, :tutor_activated, :boolean, default: false
   end
 end
