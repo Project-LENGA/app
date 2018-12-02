be379d318fc35e78c4e7fef0f41127b47613ea4d
Hinatsu Kusano <hinatsukusano@gmail.com>
2018-12-02 13:49:11 +0900

    Fix some parts

--- app/controllers/student/lesson_dates_options_controller.rb
+++ app/controllers/student/lesson_dates_options_controller.rb
@@ -2,6 +2,11 @@ class Student::LessonDatesOptionsController < Student::Base
 
   def index
     @tutor = Tutor.find(params[:tutor_id])
+    @lesson_dates_options = @tutor.lesson_dates_option
+  end
+
+  def show
+    @tutor = Tutor.find(params[:tutor_id])
     @lesson_dates_options = @tutor.lesson_dates_options
   end
 end
